c4a23b9d15d972124c5846fdc934f833528b9aec
yangxiaoyan20 <18281607011@163.com>
2017-08-26 18:59:42 +0800

    滴滴打车2017年秋招笔试题：输出第index大的数

--- indexMaxValue.py
+++ indexMaxValue.py
@@ -5,13 +5,14 @@ import sys
 #输入一个数index，值范围在0与一串数的长度之间
 #输出：对这一串数排序，输出第index大的值
 if __name__ == "__main__":
-	n = list(sys.stdin.readline().strip().split())
-	sumMax=n[0]
-	for i in xrange(len(n)):
-		sumMax2=0
-		for j in xrange(i,len(n)):
-			sumMax2=int(sumMax2)+int(n[j])
-			if(sumMax<sumMax2):
-				sumMax=sumMax2
-				
-	print("%s" %sumMax)
+	myList=map(int,list(sys.stdin.readline().strip().split()))
+	resList=[]
+	lens=len(myList)
+	index=int(sys.stdin.readline().strip())
+	for i in range(lens):
+		maxNum=max(myList)
+		resList.append(maxNum)
+		if(i==(index-1)):
+			print maxNum
+		myList.remove(maxNum)
+	#myList.sort()
